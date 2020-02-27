a546035e9cce8d9483e486d4a94e82bc7a618b1b
Arne Rademacker <arne.rademacker@fn.de>
2020-02-27 20:30:24 +0100

    Improved Script

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -4,26 +4,38 @@ const gravityConstant = 400
 const speedLimit = 500
 const speedFactor = 5
 const upDirection = Vector2(0, -1)
+const floorMaxAngle = 0.8
 
 var jumping = false
-
 var velocity = Vector2()
 var gravity = Vector2(0, gravityConstant)
 
-func _physics_process(delta):
-	if Input.is_action_pressed('ui_right'):
+func _physics_process(_delta):
+	var left = Input.is_action_pressed('ui_left')
+	var right = Input.is_action_pressed('ui_right')
+	var up = Input.is_action_pressed('ui_up')
+	
+	if right:
 		velocity.x += (speedLimit - velocity.x) / speedFactor
-	elif Input.is_action_pressed('ui_left'):
+	elif left:
 		velocity.x -= (speedLimit + velocity.x) / speedFactor
 	else:
 		velocity.x = (velocity.x / speedFactor)
 		if velocity.x < 1:
 			velocity.x = 0
 
-	if Input.is_action_pressed('ui_up'):
+	if up:
 		velocity.y = -1000
 
-	move_and_slide(velocity + gravity, upDirection)
+	# move_and_slide(
+	#	velocity vector, up direction, slide on slopes?,
+	#	maximum number of bounces per calculation,
+	#	maximum angle to count as floor in radians,
+	#	infinite inertia?
+	# )
+	var movement = move_and_slide(velocity + gravity, upDirection, true, 4, floorMaxAngle, true)
+	var posLabel = get_node('/root/Game/PositionLabel')
+	posLabel.text = str(round(movement.x)) + ':' + str(round(movement.y))
 	var collision = get_slide_count() > 0
 	if collision and is_on_floor():
 		gravity.y = gravityConstant
@@ -33,23 +45,24 @@ func _physics_process(delta):
 		jumping = true
 		gravity.y += 40
 
-func _process(delta):
-	$AnimatedSprite.offset.y = 0
-	if jumping:
-		if Input.is_action_pressed('ui_right'):
-			$AnimatedSprite.flip_h = false
-		elif Input.is_action_pressed('ui_left'):
-			$AnimatedSprite.flip_h = true
-		$AnimatedSprite.play("Jump")
-	elif Input.is_action_pressed('ui_right'):
-		$AnimatedSprite.flip_h = false
-		$AnimatedSprite.play("Walk")
-	elif Input.is_action_pressed('ui_left'):
+func _process(_delta):
+	var left = Input.is_action_pressed('ui_left')
+	var right = Input.is_action_pressed('ui_right')
+	var down = Input.is_action_pressed('ui_down')
+
+	if left:
 		$AnimatedSprite.flip_h = true
+	elif right:
+		$AnimatedSprite.flip_h = false
+
+	$AnimatedSprite.offset.y = 0
+
+	if jumping:
+		$AnimatedSprite.play("Jump")
+	elif right || left:
 		$AnimatedSprite.play("Walk")
-	elif Input.is_action_pressed('ui_down'):
+	elif down:
 		$AnimatedSprite.play("Duck")
 		$AnimatedSprite.offset.y = 22
 	else:
 		$AnimatedSprite.play("Idle")
-
