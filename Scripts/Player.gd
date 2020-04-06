760c314424e83b3e9f787ac5da4fb06d8e3283f3
Arne Rademacker <arne.rademacker@fn.de>
2020-04-06 17:40:04 +0200

    New walking scripts and tileset example

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,29 +1,53 @@
 extends KinematicBody2D
 
-const gravityConstant = 400
+const gravity = 100
+const acceleration = 50
 const speedLimit = 500
-const speedFactor = 5
+const fallingSpeedLimit = 1200
 
-var jumping = false
-var velocity = Vector2()
-var gravity = Vector2(0, gravityConstant)
+var jumping = true
+
+var input = Vector2(0, 0)
+var velocity = Vector2(0, 0)
+#var gravity = Vector2(0, gravityConstant)
 
 func _physics_process(_delta):
+	# Map inputs to velocity
 	var left = Input.is_action_pressed('ui_left')
 	var right = Input.is_action_pressed('ui_right')
 	var up = Input.is_action_pressed('ui_up')
+	var down = Input.is_action_pressed('ui_down')
 
+	input.x = 0
 	if right:
-		velocity.x += (speedLimit - velocity.x) / speedFactor
+		input.x += 1
 	elif left:
-		velocity.x -= (speedLimit + velocity.x) / speedFactor
+		input.x -= 1
+
+	if left or right:
+		velocity.x += input.x * acceleration
+		if velocity.x > speedLimit:
+			velocity.x = speedLimit
+		elif velocity.x < -speedLimit:
+			velocity.x = -speedLimit
 	else:
-		velocity.x = (velocity.x / speedFactor)
-		if velocity.x < 1:
-			velocity.x = 0
+		velocity.x -= velocity.x / (acceleration * 0.1)
 
 	if up:
-		velocity.y = -1500
+		if not jumping:
+			velocity.y -= 1500
+
+	#Map gravity to velocity
+
+	if jumping:
+		velocity.y += gravity
+	else:
+		velocity.y = gravity
+	
+	if velocity.y > fallingSpeedLimit:
+		velocity.y = fallingSpeedLimit
+
+	# Move
 
 	# move_and_slide(
 	#	velocity vector, up direction, slide on slopes?,
@@ -31,19 +55,40 @@ func _physics_process(_delta):
 	#	maximum angle to count as floor in radians,
 	#	infinite inertia?
 	# )
-	var movement = move_and_slide(velocity + gravity, Vector2(0, -1), true, 2, 0.8, true)
-	$Camera2D/DebugLabel.text = str(round(movement.x)) + ':' + str(round(movement.y))
+	var movement = move_and_slide(velocity, Vector2(0, -1), true, 2, 0.8, true)
+
+#	if right:
+#		velocity.x += (speedLimit - velocity.x) / speedFactor
+#	elif left:
+#		velocity.x -= (speedLimit + velocity.x) / speedFactor
+#	else:
+#		velocity.x = (velocity.x / speedFactor)
+#		if velocity.x < 1:
+#			velocity.x = 0
+
+#	if up:
+#		velocity.y = -1500
+
+	# move_and_slide(
+	#	velocity vector, up direction, slide on slopes?,
+	#	maximum number of bounces per calculation,
+	#	maximum angle to count as floor in radians,
+	#	infinite inertia?
+	# )
+#	var movement = move_and_slide(velocity + gravity, Vector2(0, -1), true, 2, 0.8, true)
+	$Camera2D/DebugLabel.text = 'x: ' + str(round(movement.x)) + '\n' + \
+								'y: ' + str(round(movement.y))
 
 	var collision = get_slide_count() > 0
-	if collision and is_on_floor():
-		gravity.y = gravityConstant
-		velocity.y = 0
+	if collision:
 		jumping = false
 	else:
 		jumping = true
-		gravity.y += 50
 
 func _process(_delta):
+	if Input.is_action_pressed('ui_reset'):
+		get_tree().reload_current_scene()
+
 	var left = Input.is_action_pressed('ui_left')
 	var right = Input.is_action_pressed('ui_right')
 	var down = Input.is_action_pressed('ui_down')
